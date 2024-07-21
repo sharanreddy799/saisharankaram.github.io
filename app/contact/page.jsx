e7e17a1a01b02a63b5dcec12ca9d36263eb46fd0
sharan <sharanreddy799@gmail.com>
2024-07-21 04:03:20 +0000

    Finished Contact page and commented out projects - As is still developing

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,96 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { FaPhoneAlt, FaEnvelope, FaMapMarkedAlt } from "react-icons/fa";
+const info = [
+  { icon: <FaPhoneAlt />, title: "Phone", description: "" },
+
+  { icon: <FaEnvelope />, title: "Email", description: "youremail@email.com" },
+
+  { icon: <FaMapMarkedAlt />, title: "Address", description: "" },
+];
+
+import { easeIn, motion } from "framer-motion";
+
 const Contact = () => {
-  return <div>Contact page</div>;
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: easeIn },
+      }}
+      className="py-6"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          {/**Form */}
+          <div className="xl:h-[54%] w-full order-2 xl:order-none">
+            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+              <h3 className="text-4xl text-accent">Let's work together</h3>
+              <p className="text-white/60">Why you should work with me</p>
+              {/**input */}
+              <div className="grid grid-col-1 md:grid-cols-2 gap-6">
+                <Input type="Firstname" placeholder="First name" />
+                <Input type="Lastname" placeholder="Last name" />
+                <Input type="EmailAddress" placeholder="Email Address" />
+                <Input type="PhoneNumber" placeholder="Phone Number" />
+              </div>
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="select a service" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Select a service</SelectLabel>
+                    <SelectItem value="full">Full-Time</SelectItem>
+                    <SelectItem value="part">Part-Time</SelectItem>
+                    <SelectItem value="temp">Contract</SelectItem>
+                    <SelectItem value="others">Others</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              {/**text area*/}
+              <Textarea
+                className="h-[200px]"
+                placeholder="Type your message here."
+              />
+              <Button>Send Message</Button>
+            </form>
+          </div>
+          {/**info */}
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => {
+                return (
+                  <li key={index} className="flex items-center gap-6">
+                    <div className="w-[53px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                      <div className="text-[28px]">{item.icon}</div>
+                    </div>
+                    <div className="flex-1">
+                      <p className="text-white/60">{item.title}</p>
+                      <h3 className="text-xl">{item.description}</h3>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default Contact;
--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,3 +1,21 @@
+"use client";
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import Link from "next/link";
+import Image from "next/image";
+
+const projects = [
+  {
+    nums: "01",
+    categoty: "AI",
+    title: "CelebLikeMe",
+    description: "",
+    stack: [{ name: "python" }, { name: "CNN" }, { name: "" }],
+  },
+];
 const Projects = () => {
   return <div> projects page</div>;
 };
