555b038cc919bdb82da5d500c82f2e7b3492d927
sharan <sharanreddy799@gmail.com>
2024-07-31 06:52:40 +0000

    Added state check so that, no need to close the mobile nav everytime.

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -4,6 +4,8 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { CiMenuFries } from "react-icons/ci";
+import { useState } from "react";
+
 const links = [
   {
     name: "home",
@@ -22,29 +24,44 @@ const links = [
     path: "/contact",
   },
 ];
+
 const MobileNav = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const pathname = usePathname();
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeSidebar = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <Sheet>
-      <SheetTrigger className="flex justify-center items-center">
-        <CiMenuFries className="text-[32px] text-accent" />
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
+      <SheetTrigger asChild>
+        <button
+          className="flex justify-center items-center"
+          onClick={toggleSidebar}
+        >
+          <CiMenuFries className="text-[32px] text-accent" />
+        </button>
       </SheetTrigger>
       <SheetContent className="flex flex-col">
-        {/** logo */}
         <div className="mt-32 mb-40 text-center text-2xl">
-          <Link href={"/"}>
+          <Link href={"/"} onClick={closeSidebar}>
             <h1 className="text-3xl font-semibold">
               Sharan <span className="text-accent text-xl">&lt; /.&gt;</span>
             </h1>
           </Link>
         </div>
-        {/** nav */}
         <nav className="flex flex-col justify-center items-center gap-8">
           {links.map((link, index) => {
             return (
               <Link
                 href={link.path}
                 key={index}
+                onClick={closeSidebar}
                 className={`${
                   link.path === pathname &&
                   "text-accent border-b-2 border-accent"
