f2fc70aff7a42d8aba52c454b288e658089801b0
sharan <sharanreddy799@gmail.com>
2024-09-08 21:01:58 +0000

    Updated Sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,12 +1,7 @@
 module.exports = {
-  siteUrl: "https://saisharankaram-github-io.vercel.app",
+  siteUrl: "https://saisharankaram-github-io.vercel.app", // Replace with your actual domain
   generateRobotsTxt: true, // Generate a robots.txt file with the sitemap included
-  sitemapSize: 7000, // Split sitemap into multiple files if you have more URLs
-  outDir: "./public", // Where to save sitemaps (relative to the base directory)
-  robotsTxtOptions: {
-    policies: [{ userAgent: "*", allow: "/" }],
-    additionalSitemaps: [
-      "https://saisharankaram-github-io.vercel.app/sitemap.xml", // Main sitemap
-    ],
-  },
+  autoLastmod: true, // Automatically append last modification time to URLs
+  // Exclude specific paths or patterns if necessary
+  exclude: ["/api/*"], // Example: Exclude all API routes
 };
