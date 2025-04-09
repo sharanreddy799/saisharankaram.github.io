67eb9bc2967670ecb10ac43b57382e1fdcc21535
Sai Sharan Karam <42332628+sharanreddy799@users.noreply.github.com>
2025-04-09 09:33:39 -0500

    Update page.jsx

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -30,7 +30,7 @@ const experience = {
     {
       company: "Wedigma",
       position: "Software Developer",
-      duration: "2023 - 2024",
+      duration: "2022 - 2024",
       details2: [
         {
           jobTitle: "Software Developer",
